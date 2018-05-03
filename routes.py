588bb72f331dcb4227c5dedf2a8eb8f20811d8f4
Dan Russell-Brain <daniel.rb@hotmail.co.uk>
2018-05-03 11:12:37 +0100

    Signup form added

--- routes.py
+++ routes.py
@@ -1,7 +1,13 @@
 from flask import Flask, render_template
-
+from models import db
+from forms import SignupForm
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/learningflask'
+db.init_app(app)
+
+app.secret_key = 'development-key'
 
 
 @app.route('/')
@@ -12,6 +18,12 @@ def index():
 @app.route('/about')
 def about():
     return render_template('about.html')
+
+
+@app.route('/signup')
+def signup():
+    form = SignupForm()
+    return render_template('signup.html', form=form)
 
 
 if __name__ == '__main__':
